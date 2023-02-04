202e891741fcd2f71dfb8d11b7715c79de5b2b75
Brett Polacek <brettpolacek@gmail.com>
2023-02-04 18:46:02 -0500

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+ dodger.style.backgroundColor = "#FF69B4";
+ dodger.style.left = "150px";
+
+ //moving left START//
+ document.addEventListener("keydown", function(e){
+    if(e(key)==="ArrowLeft") {
+        moveDodgerLeft();
+ }
+});
+
+ function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+ }
+
+
+ //Moving Right START
+
+ document.addEventListener("keydown", function(e){
+    if(e(key)==="ArrowRight") {
+        moveDodgerRight();
+ }
+});
+
+ function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left + 1}px`;
+ }
